9acc5c2ab46ea5c6040ccd2419abf919abe58c97
Rohit Sharma <fakeeclipx@gmail.com>
2025-03-09 22:40:53 +0530

    `Added data serialization, CSV, datetime, time, and regular expression examples to standard library notebook.`

---
+++ import create module/package/_init_.py
@@ -0,0 +1,4 @@
+###  special file that is used to define and package  and initializetheir namespace
+
+
+
---
+++ import create module/test for package.py
@@ -0,0 +1,7 @@
+from package.math import*
+
+print(addition(4,5))
+print(sub(4,5))
+
+from package.subpackage.mult import multiply
+print(multiply(4,5))
